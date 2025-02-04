1a6ff26fbd34360d475326e9f6cedea5531c263d
Melissa <>
2025-02-04 09:41:14 -0800

    Tested successfully using Melissa object for 2025.Q1

--- BuildRightFielderNETdll.ps1
+++ BuildRightFielderNETdll.ps1
@@ -18,7 +18,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2024.Q4'
+$RELEASE_VERSION = '2025.Q1'
 
 # Uses the location of the .ps1 file 
 $CurrentPath = $PSScriptRoot
